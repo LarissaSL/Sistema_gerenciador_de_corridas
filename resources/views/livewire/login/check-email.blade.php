5a749c9a0ee0e53330a7a06ab0bef7c4fd71f00e
LarissaSL <larissa.silvaedge@gmail.com>
2025-03-11 17:39:39 -0300

    feat: add email input with validation for admin check in login view

---
+++ resources/views/livewire/login/check-email.blade.php
@@ -0,0 +1,11 @@
+<div>
+    {{-- Manter o Name e as tags de wire --}}
+    <label for="email" class="form-label">Email</label>
+    <input type="email" id="email" name="email" wire:model="email" wire:blur="checkEmail" class="form-control {{ $isAdmin ? '' : 'is-invalid' }}" placeholder="Digite seu e-mail">
+    
+    @if(!$isAdmin)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @endif
+</div>
