02a1840351cafff6c71aba94090467b298a0bbd4
LarissaSL <larissa.silvaedge@gmail.com>
2025-03-18 14:36:08 -0300

    feat: implement Authenticator class for user authentication handling

---
+++ app/Application/Login/Login/Authenticator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Application\Login\Login;
+
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class Authenticator
+{
+    public function execute($credentials)
+    {
+        return $this->autentic($credentials);
+    }
+
+    private function autentic($credentials)
+    {
+        try {
+            $autenticado = Auth::attempt($credentials);
+    
+            return $autenticado;
+
+        } catch (\Exception $e) {
+            Log::error('Erro durante a autenticação: ' . $e->getMessage());
+            throw new Exception('Erro durante a autenticação: ' . $e->getMessage());
+    
+            return false;
+        }
+    }
+}
