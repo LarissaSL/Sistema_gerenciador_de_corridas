d8c4dc01cce512a87d8f2bfa34490f887389904f
LarissaSL <larissa.silvaedge@gmail.com>
2025-04-22 16:01:17 -0300

    feat: add templates and scripts for dashboard data visualization

---
+++ public/assets/js/js-evolutionAnnual.js
@@ -0,0 +1,5 @@
+
+// Rota para obter os dados via JSON, substituir o ano e o mês se assim for necessário
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Rota para os dados do Gráfico 1:', window.evolutionAnnualRoute);
+});
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,7 +1,11 @@
-@extends('template.temporaryLoginTemplate')
+@extends('template.appTemplate')
+
+@section('title')
+    {{ $title }}
+@endsection
 
 @section('content')
-    <a href="{{ route('login.home') }}">
+    <a href="">
         <ion-icon name="arrow-back-outline"></ion-icon>
     </a>
 
@@ -22,11 +26,24 @@
         @endforeach
     @endif
 
-
-    <h3>Seja bem-vindo(a) {{ $userName }}</h3>
+    <h3>Bem-vindo de volta!</h3>
+    
+    <a href="">Consultar lista de pilotos cadastrados</a>
+    <a href="">Cadastrar uma nova corrida</a>
+    <a href="">Cadastrar um novo campeonato</a>
 
 @endsection
 
+
 @section('scripts')
 
+    <script>
+       window.evolutionAnnualRoute = "{{ route('dashboard.evolution.annual', ['year' => 2025, 'month' => '']) }}";
+       window.financeDataRoute = "{{ route('dashboard.finance.data') }}";
+       window.racesDataRoute = "{{ route('dashboard.races.data') }}";
+    </script>
+
+    <script src={{ URL::asset('assets/js/js-evolutionAnnual.js') }}></script>
+    <script src={{ URL::asset('assets/js/js-financeData.js') }}></script>
+    <script src={{ URL::asset('assets/js/js-racesData.js') }}></script>
 @endsection
